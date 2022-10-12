698ea46a4e3b3bce99e2d210bb0f09bede089163
SeverinSchmidt <78699853+SeverinSchmidt@users.noreply.github.com>
2022-10-12 14:39:27 +0200

    Extended the program's entry processing, and added feature to print out average population of each city.

--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
@@ -3,6 +3,8 @@ package edu.sdse.csvprocessor;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class CityCSVProcessor {
 	
@@ -13,6 +15,9 @@ public class CityCSVProcessor {
 			br.readLine();
 			
 			String line;
+			ArrayList<CityRecord> allRecords = new ArrayList<CityRecord>();
+			Map<String, ArrayList<CityRecord>> recordsByCity = new HashMap<String, ArrayList<CityRecord>>();
+			HashSet<String> uniqueCities = new HashSet<String>();
 			
 			while ((line = br.readLine()) != null) {
 				// Parse each line
@@ -28,7 +33,56 @@ public class CityCSVProcessor {
 				//TODO: Extend the program to process entries!
 				CityRecord cityRecord = new CityRecord(id, year, city, population);
 				System.out.println(cityRecord);
+				allRecords.add(cityRecord);
+				uniqueCities.add(city);
+
+			/* for unique city
+		     
+		     * for each record in allRecords add the ones where city == unique city to a temporary list of records
+		     * Then create add key and value (list of records) to Map
+		     */
+		
+				}
+			for (String c : uniqueCities) {
+				ArrayList<CityRecord> temp = new ArrayList<CityRecord>();
+				for (CityRecord r : allRecords) {
+					if (r.city.equals(c)) {
+						temp.add(r);
+					}
+				}
+				recordsByCity.put(c, temp);
 			}
+			/*
+			 * for each city
+			 * for each record in city:
+			 * 	count entries
+			 * 	add up population
+			 *  if minimum year
+			 *  if maximum year
+			 *  
+			 * divide to get avg pop
+			 * print out the values
+			 *
+			 */
+			for (Entry<String, ArrayList<CityRecord>> entry : recordsByCity.entrySet()) {
+				int count = 0;
+				int totalPop = 0;
+				int minYear = 10000;
+				int maxYear = 0;
+				for (CityRecord r : entry.getValue()) {
+					count += 1;
+					totalPop += r.population;
+					if (r.year < minYear) {
+						minYear = r.year;
+					} else if (r.year > maxYear) {
+						maxYear = r.year;
+					}
+				}
+				System.out.println("Average population of " + entry.getKey() + " (" + minYear + "-" + maxYear + "; " + count + "): " + totalPop / count);
+			}
+			
+				
+			
 		} catch (Exception e) {
 			System.err.println("An error occurred:");
 			e.printStackTrace();
